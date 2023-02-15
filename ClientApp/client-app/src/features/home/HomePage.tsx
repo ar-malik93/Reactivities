debb4f58a8c41fa915a6b01ffc97c8f96c975d65
Abdurrehman Malik <malikabdurrehman89@gmail.com>
2023-02-16 04:44:43 +0500

    Added styles to the components

--- ClientApp/client-app/src/features/home/HomePage.tsx
+++ ClientApp/client-app/src/features/home/HomePage.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Container } from 'semantic-ui-react'
+import { Container, Header, Segment,Image, Button } from 'semantic-ui-react'
 
 
 export default function HomePage() {
     return(
-        <Container>
-            <h2>Home Page</h2>
-            <h3>Go to the <Link to={'/activities'}>Activities</Link></h3>
-        </Container>
+        <Segment inverted textAlign='center' vertical className='masthead'>
+            <Container text>
+                <Header as='h1' inverted>
+                    <Image size='massive' src='/assets/logo.png' alt='logo' style={{marginBottom: 12}} />
+                    Reactivities
+                </Header>
+                <Header as='h2' inverted content='Welcome to Reactivities' />
+                <Button as={Link} to='/activities' content='Take me to Activities!' size='huge'/>
+            </Container>
+        </Segment>
     )
 }
--- ClientApp/client-app/src/index.tsx
+++ ClientApp/client-app/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import 'semantic-ui-css/semantic.min.css'
+import 'react-calendar/dist/Calendar.css'
 import App from './app/layout/App';
 import './app/layout/styles.css';
 import { router } from './app/router/routes';
